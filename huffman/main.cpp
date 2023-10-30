28f7a453d5c2bd7c583924c5db89c38dbd269e38
wiaderniczy <maciej@tomczuk.eu>
2023-10-30 20:35:12 +0100

    v2 xd

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <map>
 #include <fstream>
@@ -6,10 +7,10 @@
 #include <bitset>
 #include <algorithm>
 
-std::string readFromFile(std::string * filename){
+std::string readFromFile(std::string *filename) {
     std::ifstream file;
     file.open(*filename, std::ios::in);
-    if (!file.good()){
+    if (!file.good()) {
         std::cout << "No input file";
         return "";
     }
@@ -18,7 +19,7 @@ std::string readFromFile(std::string * filename){
     return text;
 }
 
-void encode(const std::string text){
+void encode(const std::string text) {
     int iterator = 0;
     std::map<char, int> alphabet = {};
     for (int i = 0; i < text.size(); i++) {
@@ -29,7 +30,7 @@ void encode(const std::string text){
     }
 
     std::vector<std::pair<char, int>> sortedVector(alphabet.begin(), alphabet.end());
-    std::sort(sortedVector.begin(), sortedVector.end(), [](const auto& a, const auto& b) {
+    std::sort(sortedVector.begin(), sortedVector.end(), [](const auto &a, const auto &b) {
         return a.second < b.second;
     });
 
@@ -44,43 +45,43 @@ void encode(const std::string text){
     }
 
     std::string additionFirstBits = "";
-    for (auto i = 0; i < addition; i++ ){
+    for (auto i = 0; i < addition; i++) {
         additionFirstBits += "1";
     }
 
     std::bitset<8> additionRep(addition);
     std::string encodedString = additionFirstBits;
-    for (auto i = 0; i < text.size(); i++){
+    for (auto i = 0; i < text.size(); i++) {
         std::bitset<32> rep(alphabet.at(text[i]));
         encodedString += rep.to_string().substr(32 - N);
     }
 
 
-    int outputSize = iterator + 1 + finalBitSize/8;
+    int outputSize = iterator + 1 + finalBitSize / 8;
     char output[outputSize] = {};
     output[0] = iterator;
     for (int i = 0; i < sortedVector.size(); i++) {
-        output[i+1] = sortedVector.at(i).first;
+        output[i + 1] = sortedVector.at(i).first;
     }
-    int guard = encodedString.length()/8;
-    for (int i = 0; i < guard; i++){
+    int guard = encodedString.length() / 8;
+    for (int i = 0; i < guard; i++) {
         output[1 + sortedVector.size() + i] = std::stoi(encodedString.substr(0, 8), nullptr, 2);
-        encodedString.erase(0,8);
+        encodedString.erase(0, 8);
     }
     std::ofstream out;
     out.open("encoded", std::ios::trunc);
-    for (char x: output){
+    for (char x: output) {
         out << x;
     }
     out.close();
 }
 
-void decode(std::string text){
-    int alphabetLength = (int)text[0];
+void decode(std::string text) {
+    int alphabetLength = (int) text[0];
 
     std::map<int, char> alphabet = {};
     for (int i = 0; i < alphabetLength; i++) {
-        alphabet[i] = text[1+i];
+        alphabet[i] = text[1 + i];
     }
     text.erase(0, alphabetLength + 1);
 
@@ -88,7 +89,7 @@ void decode(std::string text){
 
     std::vector<bool> bits;
 
-    for (char c : text) {
+    for (char c: text) {
         std::bitset<8> charBits(c);
         for (int i = 7; i >= 0; --i) {
             bits.push_back(charBits[i]);
@@ -103,16 +104,15 @@ void decode(std::string text){
 
     std::string decodedString = "";
 
-    for (bool bit: bits){
+    for (bool bit: bits) {
         decodedString += (bit ? '1' : '0');
     }
     std::string final = "";
-    for (int i = 0; i < decodedString.size(); i+=N){
+    for (int i = 0; i < decodedString.size(); i += N) {
         std::string chunk = decodedString.substr(i, N);
-        int value = std::stoi(chunk, nullptr,  2);
+        int value = std::stoi(chunk, nullptr, 2);
         final += std::string(1, alphabet.at(value));
     }
-    std::cout<<final;
 
     std::ofstream out;
     out.open("decoded.txt", std::ios::trunc);
@@ -129,7 +129,7 @@ int main() {
     std::cin >> option;
     text = readFromFile(&file);
 
-    switch(option) {
+    switch (option) {
         case 1:
             decode(text);
             break;
@@ -139,3 +139,7 @@ int main() {
     }
     return 0;
 }
+
+/*na zajęciach niestety musiałem zająć się postawieniem środowiska na swoim laptopie,
+ale też zapoznawałem się z różnymi metodami szyfrowania, przeglądałem możliwe opcje 
+oraz zastanawiałem się w jaki sposób je zaimplementować*/
